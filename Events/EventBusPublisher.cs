30e2b41b7ef0a781fcdbd02d60391984a4ec9ca9
Gytis <medelis.gytis@gmail.com>
2026-03-06 23:42:02 +0200

    Add project files.

---
+++ Events/EventBusPublisher.cs
@@ -0,0 +1,28 @@
+﻿using Mono.Cecil;
+using OutwardChatCommandsTemplate.Events.Publishers;
+using OutwardChatCommandsTemplate.Managers;
+using OutwardChatCommandsTemplate.Utility.Enums;
+using OutwardChatCommandsTemplate.Utility.Helpers;
+using OutwardModsCommunicator.EventBus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutwardChatCommandsTemplate.Events
+{
+    public static class EventBusPublisher
+    {
+        public const string Event_AddCommand = "ChatCommandsManager@AddChatCommand";
+        public const string Event_RemoveCommand = "ChatCommandsManager@RemoveChatCommand";
+
+        //other mods listener uid
+        public const string ChatCommands_Listener = "gymmed.chat_commands_manager_*";
+
+        public static void SendCommands()
+        {
+            ExamplePublisher.SendExampleCommand();
+        }
+    }
+}
